06fcf97f254740522214a2598249b5ceb2ba2527
Brett Swift <brettswift@gmail.com>
2015-02-20 15:06:59 -0700

    optimized r10k runs - left old shell script as alternative

--- manifests/r10k_modules.pp
+++ manifests/r10k_modules.pp
@@ -1,7 +1,18 @@
 exec {'r10k' :
 	command 		=> 'r10k -v debug puppetfile install > r10k.log',
 	path	 			=> ['/bin','/usr/bin','/opt/puppet/bin'],
-	cwd 				=> '/vagrant'
+	cwd 				=> '/vagrant',
+	unless 			=> "diff /tmp/puppetfile_hash_new /tmp/puppetfile_hash_old",
+}
+
+exec {'create new hash':
+	command => '/usr/bin/md5sum /vagrant/Puppetfile > /tmp/puppetfile_hash_new',
+}
+
+exec {'create old hash':
+	command => '/usr/bin/md5sum /vagrant/Puppetfile > /tmp/puppetfile_hash_old',
+	subscribe   => Exec["r10k"],
+	refreshonly => true
 }
 
 package { "r10k":
@@ -16,4 +27,5 @@ package { "git":
 notify {"starting r10k deployment": }->
 Package['git']->
 Package['r10k']->
+Exec['create new hash']->
 Exec['r10k']
